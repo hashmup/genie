442313164fa95a14db110955a2ae922fced4f52a
hashmup <hashmup@gmail.com>
2017-12-14 14:02:30 +0900

    Implement ode functions

--- genie/transpiler/neuron/ode.py
+++ genie/transpiler/neuron/ode.py
@@ -1,5 +1,6 @@
 from jinja2 import Environment, FileSystemLoader
 from os.path import join, dirname, abspath
+from textx.model import children_of_type, parent_of_type
 
 
 ROOT = abspath(dirname(__file__))
@@ -14,10 +15,39 @@ class ODE():
             "ode_func.c"
         )
 
-    def parse(self, root):
-        return {}
+    def gen(self, root):
+        return {
+            "num_states": self.get_num_states(root),
+            "read_ions": self.get_read_ions(root),
+            "ode_spec1": self.get_ode_spec1(root),
+            "ode_matsol1": self.get_ode_matsol1(root)
+        }
+
+    def get_num_states(self, root):
+        return "{0}".format(len(children_of_type('State', root)[0].state_vars))
+
+    def get_read_ions(self, root):
+        code = ""
+        for x in children_of_type('UseIon', root):
+            code += "\t\t{0} = _ion_{0};\n"\
+                    .format(x.r[0].reads[0].name)
+        return code
+
+    def get_ode_spec1(self, root):
+        code = ""
+        for state in children_of_type('State', root)[0].state_vars:
+            code += "\tD{0} = ({0}inf - {0}) / {0}tau;\n"\
+                    .format(state.name)
+        return code
+
+    def get_ode_matsol1(self, root):
+        code = ""
+        for state in children_of_type('State', root)[0].state_vars:
+            code += "\tD{0} = D{0} / (1. + dt / {0}tau);\n"\
+                    .format(state.name)
+        return code
 
     def compile(self, filename, root):
-        tokens = self.parse(root)
+        tokens = self.gen(root)
         tokens.update({"filename": filename}.items())
         return self.jinja_template.render(**tokens)
--- genie/transpiler/neuron/template/ode_func.c
+++ genie/transpiler/neuron/template/ode_func.c
@@ -1,5 +1,5 @@
 static int _ode_count(int _type){
-  return 3;
+  return {{ num_state }};
 }
 
 static void _ode_map(int _ieq, double** _pv, double** _pvdot, double* _pp, Datum* _ppd, double* _atol, int _type) {
@@ -9,7 +9,7 @@ static void _ode_map(int _ieq, double** _pv, double** _pvdot, double* _pp, Datum
   _p = _pp;
   _ppvar = _ppd;
  	_cvode_ieq = _ieq;
- 	for (_i=0; _i < 3; ++_i) {
+ 	for (_i=0; _i < {{ num_state }}; ++_i) {
  	  _pv[_i] = _pp + _slist1[_i];
     _pvdot[_i] = _pp + _dlist1[_i];
  		_cvode_abstol(_atollist, _atol, _i);
@@ -30,8 +30,7 @@ static void _ode_spec(_NrnThread* _nt, _Memb_list* _ml, int _type) {
     _ppvar = _ml->_pdata[_iml];
     _nd = _ml->_nodelist[_iml];
     v = NODEV(_nd);
-    ena = _ion_ena;
-    ek = _ion_ek;
+{{ read_ions }}
     _ode_spec1(_p, _ppvar, _thread, _nt);
   }
 }
@@ -42,9 +41,7 @@ static int _ode_spec1(double* _p,
     _NrnThread* _nt) {
   int _reset = 0;
   rates(_threadargscomma_ v);
-  Dm = (minf - m) / mtau;
-  Dh = (hinf - h) / htau;
-  Dn = (ninf - n) / ntau;
+{{ ode_spec1 }}
   return _reset;
 }
 
@@ -62,8 +59,7 @@ static void _ode_matsol(_NrnThread* _nt, _Memb_list* _ml, int _type) {
     _ppvar = _ml->_pdata[_iml];
     _nd = _ml->_nodelist[_iml];
     v = NODEV(_nd);
-    ena = _ion_ena;
-    ek = _ion_ek;
+{{ read_ions }}
     _ode_matsol1(_p, _ppvar, _thread, _nt);
   }
 }
@@ -73,8 +69,6 @@ static int _ode_matsol1(double* _p,
     Datum* _thread,
     _NrnThread* _nt) {
   rates (_threadargscomma_ v);
-  Dm = Dm / (1. - dt*(((( - 1.0 ))) / mtau));
-  Dh = Dh / (1. - dt*(((( - 1.0 ))) / htau));
-  Dn = Dn / (1. - dt*(((( - 1.0 ))) / ntau));
+{{ ode_matsol1 }}
   return 0;
 }
