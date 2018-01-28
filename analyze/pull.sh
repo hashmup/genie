6485fc61dbf50ab360d8c4c9fb82b3c317c4220c
hashmup <hashmup@gmail.com>
2018-01-29 00:32:00 +0900

    add analyze dir

--- analyze/pull.sh
+++ analyze/pull.sh
@@ -1,2 +1,6 @@
-mkdir -p data
-scp -r cluster:~/genie/data/* data/
+mkdir -p cluster
+mkdir -p k
+# scp -r cluster:~/genie/data/*/result* data/
+# rsync -a --exclude=~/genie/data/*/tmp cluster:~/genie/data data/
+rsync -av  --include="result*.csv" --include="job*.json" --include="*/" --exclude="*" --exclude="tmp/" cluster:~/genie/data cluster/
+rsync -av  --include="result*.csv" --include="job*.json" --include="*/" --exclude="*" --exclude="tmp/" kkk:~/genie/data k/
